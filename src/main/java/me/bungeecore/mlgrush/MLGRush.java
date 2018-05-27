87e5f94fa0aa2932de0a23f35a5de3ed00684430
Justin Klein <bungeecore@minecor.de>
2018-05-28 01:37:29 +0200

    Anfang des Lobbycountdowns

--- src/main/java/me/bungeecore/mlgrush/MLGRush.java
+++ src/main/java/me/bungeecore/mlgrush/MLGRush.java
@@ -6,6 +6,8 @@ public class MLGRush extends JavaPlugin {
 
     public static MLGRush instance;
 
+    public static String prefix = "§7| §9§bMLGRush §7| ";
+
     @Override
     public void onEnable() {
         instance = this;
--- src/main/java/me/bungeecore/mlgrush/utils/LobbyCountdown.java
+++ src/main/java/me/bungeecore/mlgrush/utils/LobbyCountdown.java
@@ -2,13 +2,27 @@ package me.bungeecore.mlgrush.utils;
 
 import me.bungeecore.mlgrush.MLGRush;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 public class LobbyCountdown {
+
+    private static int countdown = 15;
+
+    private static int TaskID = 0;
 
     public static void startCountdown() {
         Bukkit.getScheduler().scheduleAsyncRepeatingTask(MLGRush.instance, new Runnable() {
             public void run() {
+                if (countdown != 0) {
+                    countdown--;
+                }
 
+                if (countdown == 10 || countdown == 5 || countdown == 4 || countdown == 3 || countdown == 2 || countdown == 1) {
+                    Bukkit.broadcastMessage(MLGRush.prefix + "§7Das Spiel beginnt in §a" + countdown + " Sekunde/n§7...");
+                    for (Player all : Bukkit.getOnlinePlayers()) {
+                        
+                    }
+                }
             }
         }, 20, 20);
     }
