0687e01413799a5ea704162d2eae95654c7592d4
Justin Klein <bungeecore@minecor.de>
2018-05-29 00:56:15 +0200

    Fertigstellung der LocationAPI

--- src/main/java/me/bungeecore/mlgrush/MLGRush.java
+++ src/main/java/me/bungeecore/mlgrush/MLGRush.java
@@ -1,6 +1,10 @@
 package me.bungeecore.mlgrush;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public class MLGRush extends JavaPlugin {
 
@@ -8,9 +12,14 @@ public class MLGRush extends JavaPlugin {
 
     public static String prefix = "§7| §9§bMLGRush §7| ";
 
+    public static File file;
+    public static FileConfiguration cfg;
+
     @Override
     public void onEnable() {
         instance = this;
+        file = new File(instance.getDataFolder(), "locs.yml");
+        cfg = YamlConfiguration.loadConfiguration(file);
     }
 
     @Override
--- src/main/java/me/bungeecore/mlgrush/utils/LocationAPI.java
+++ src/main/java/me/bungeecore/mlgrush/utils/LocationAPI.java
@@ -1,10 +1,37 @@
 package me.bungeecore.mlgrush.utils;
 
+import me.bungeecore.mlgrush.MLGRush;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 public class LocationAPI {
 
     public static void setLocation(Player p, String loc) {
-        
+        MLGRush.cfg.set(loc + ".World" , p.getWorld().getName());
+        MLGRush.cfg.set(loc + ".X", p.getLocation().getX());
+        MLGRush.cfg.set(loc + ".Y", p.getLocation().getY());
+        MLGRush.cfg.set(loc + ".Z", p.getLocation().getZ());
+        MLGRush.cfg.set(loc + ".Yaw", p.getLocation().getYaw());
+        MLGRush.cfg.set(loc + ".Pitch", p.getLocation().getPitch());
+
+        try {
+            MLGRush.cfg.save(MLGRush.file);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static Location getLocation(String loc) {
+        World world = Bukkit.getWorld(MLGRush.cfg.getString(loc + ".World"));
+        double x = MLGRush.cfg.getDouble(loc + ".X");
+        double y = MLGRush.cfg.getDouble(loc + ".Y");
+        double z = MLGRush.cfg.getDouble(loc + ".Z");
+        float yaw = (float) MLGRush.cfg.getDouble(loc + ".Yaw");
+        float pitch = (float) MLGRush.cfg.getDouble(loc + ".Pitch");
+
+        Location location = new Location(world, x, y, z, yaw, pitch);
+        return location;
     }
 }
