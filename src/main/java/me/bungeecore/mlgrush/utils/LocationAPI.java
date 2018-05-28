8c7c83b89ff118b0c379df9dc0a836a16f5efab3
Justin Klein <bungeecore@minecor.de>
2018-05-28 18:09:30 +0200

    Anfang der LocationAPI

---
+++ src/main/java/me/bungeecore/mlgrush/utils/LocationAPI.java
@@ -0,0 +1,10 @@
+package me.bungeecore.mlgrush.utils;
+
+import org.bukkit.entity.Player;
+
+public class LocationAPI {
+
+    public static void setLocation(Player p, String loc) {
+        
+    }
+}
